0ccf55e3b41488ecf368fc723236c9c15b12a156
hinoku <klevjej@gmail.com>
2019-06-02 17:55:13 +0200

    add submodule for site

--- deploy.sh
+++ deploy.sh
@@ -2,19 +2,23 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build site
-hugo
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Add and commit changes
-cd ../hinoku.github.io
+# Go To Public folder
+cd public
+# Add changes to git.
 git add .
 
-msg="rebuild site `date`"
+# Commit changes.
+msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-git push
+# Push source and build repos.
+git push origin master
 
-cd ../my-site
+# Come Back up to the Project Root
+cd ..
